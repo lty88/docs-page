f903da876d706149048c9c036b4770d3c84134ac
lty88 <98n9@163>
2024-12-08 19:18:41 +0800

    action

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+              进入文档
           </Link>
         </div>
       </div>
